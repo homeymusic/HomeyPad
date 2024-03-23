6bcf0f7efd028d6b81e36d2750da20e640785129
Brian Mulloy <brian@landlessness.net>
2024-03-23 16:02:55 -0400

    running it back

---
+++ HomeyPad/HomeyPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeyPadApp.swift
+//  HomeyPad
+//
+//  Created by Brian McAuliff Mulloy on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct HomeyPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
