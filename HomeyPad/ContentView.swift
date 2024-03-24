fdc3f235a679bff545901822aba4d08d084ca3b8
Brian Mulloy <brian@landlessness.net>
2024-03-24 11:43:46 -0400

    iterating on keyboards

--- HomeyPad/ContentView.swift
+++ HomeyPad/ContentView.swift
@@ -5,45 +5,45 @@ import Tonic
 struct ContentView: View {
     @StateObject var viewConductor = ViewConductor()
     @Environment(\.colorScheme) var colorScheme
-
+    
     var body: some View {
+        let keyboards: [Keyboard] = [
+            Keyboard(layout: .piano(pitchRange: Pitch(intValue: viewConductor.lowNote) ... Pitch(intValue: viewConductor.highNote)),
+                     noteOn: viewConductor.noteOnWithVerticalVelocity(pitch:point:), noteOff: viewConductor.noteOff),
+            Keyboard(layout: .isomorphic(pitchRange:
+                                            Pitch(intValue: 12 + viewConductor.rootIndex) ... Pitch(intValue: 84 + viewConductor.rootIndex),
+                                         root: viewConductor.root,
+                                         scale: viewConductor.scale),
+                     noteOn: viewConductor.noteOnWithReversedVerticalVelocity(pitch:point:), noteOff: viewConductor.noteOff),
+            Keyboard(layout: .guitar(),
+                     noteOn: viewConductor.noteOn, noteOff: viewConductor.noteOff) { pitch, isActivated in
+                         KeyboardKey(pitch: pitch,
+                                     isActivated: isActivated,
+                                     text: pitch.note(in: .F).description,
+                                     pressedColor: Color(PitchColor.newtonian[Int(pitch.pitchClass)]),
+                                     alignment: .center)
+                     },
+            Keyboard(layout: .isomorphic(pitchRange: Pitch(48) ... Pitch(65))) { pitch, isActivated in
+                KeyboardKey(pitch: pitch,
+                            isActivated: isActivated,
+                            text: pitch.note(in: .F).description,
+                            pressedColor: Color(PitchColor.newtonian[Int(pitch.pitchClass)]))
+            }
+        ]
+        
         HStack {
-            Keyboard(layout: .verticalIsomorphic(pitchRange: Pitch(48) ... Pitch(77))).frame(width: 100)
+            Keyboard(layout: .verticalIsomorphic(pitchRange: Pitch(48) ... Pitch(77)))
+                .frame(width: 100)
             VStack {
                 NoteRangeStepperView(viewConductor: viewConductor)
-                Keyboard(layout: .piano(pitchRange: Pitch(intValue: viewConductor.lowNote) ... Pitch(intValue: viewConductor.highNote)),
-                         noteOn: viewConductor.noteOnWithVerticalVelocity(pitch:point:), noteOff: viewConductor.noteOff)
-                .frame(minWidth: 100, minHeight: 100)
-
                 HStack {
                     RootStepperView(viewConductor: viewConductor)
                     ScaleStepperView(viewConductor: viewConductor)
                 }
-                Keyboard(layout: .isomorphic(pitchRange:
-                                                Pitch(intValue: 12 + viewConductor.rootIndex) ... Pitch(intValue: 84 + viewConductor.rootIndex),
-                                             root: viewConductor.root,
-                                             scale: viewConductor.scale),
-                         noteOn: viewConductor.noteOnWithReversedVerticalVelocity(pitch:point:), noteOff: viewConductor.noteOff)
-                .frame(minWidth: 100, minHeight: 100)
-
-                Keyboard(layout: .guitar(),
-                         noteOn: viewConductor.noteOn, noteOff: viewConductor.noteOff) { pitch, isActivated in
-                    KeyboardKey(pitch: pitch,
-                                isActivated: isActivated,
-                                text: pitch.note(in: .F).description,
-                                pressedColor: Color(PitchColor.newtonian[Int(pitch.pitchClass)]),
-                                alignment: .center)
+                ForEach(keyboards) { keyboard in
+                    keyboard
+                        .frame(minWidth: 100, minHeight: 100)
                 }
-                .frame(minWidth: 100, minHeight: 100)
-
-                Keyboard(layout: .isomorphic(pitchRange: Pitch(48) ... Pitch(65))) { pitch, isActivated in
-                    KeyboardKey(pitch: pitch,
-                                isActivated: isActivated,
-                                text: pitch.note(in: .F).description,
-                                pressedColor: Color(PitchColor.newtonian[Int(pitch.pitchClass)]))
-                }
-                .frame(minWidth: 100, minHeight: 100)
-
                 Keyboard(latching: true, noteOn: viewConductor.noteOn, noteOff: viewConductor.noteOff) { pitch, isActivated in
                     if isActivated {
                         ZStack {
@@ -53,7 +53,7 @@ struct ContentView: View {
                                 Text(pitch.note(in: .C).description).font(.largeTitle)
                             }.padding()
                         }
-
+                        
                     } else {
                         Rectangle().foregroundColor(viewConductor.randomColors[Int(pitch.intValue) % 12])
                     }
@@ -65,10 +65,11 @@ struct ContentView: View {
                                        initialSpacerRatio: viewConductor.evenSpacingInitialSpacerRatio,
                                        spacerRatio: viewConductor.evenSpacingSpacerRatio,
                                        relativeBlackKeyWidth: viewConductor.evenSpacingRelativeBlackKeyWidth)
-            ).frame(width: 100)
+            )
+            .frame(width: 100)
         }
-        .background(colorScheme == .dark ?
-                    Color.clear : Color(red: 0.9, green: 0.9, blue: 0.9))
+        .ignoresSafeArea(edges:.horizontal)
+        .background(Color.gray)
     }
 }
 
