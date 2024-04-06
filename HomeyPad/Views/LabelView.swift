7ac46461dd938c3a7c739234e0ab750c5c4b6751
Brian Mulloy <brian@landlessness.net>
2024-04-06 07:37:49 -0400

    fixed bug in symmetric layout

--- HomeyPad/Views/LabelView.swift
+++ HomeyPad/Views/LabelView.swift
@@ -5,148 +5,134 @@ public struct LabelView: View {
     var proxySize: CGSize
     
     public var body: some View {
-        //        let padding = proxySize.width / 2.0 + 8.0
-        //        let adjustment = keyboardKey.outlineTonic ? keyboardKey.outlineBorderSize : 0.0
-        //        let _foo = print("outlineTonic \(keyboardKey.outlineTonic) intervalClass \(keyboardKey.interval.intervalClass)")
-        //        let _bar = print("adjustment \(adjustment)")
-        if keyboardKey.layoutChoice == .symmetric {
-            if keyboardKey.interval.consonanceDissonance > .consonant {
-                VStack(spacing: 0.0) {
-                    AllSymbolsView(keyboardKey: keyboardKey, proxySize: proxySize)
-                    //                        .padding(.top, padding - adjustment)
-                    //                        .padding(.bottom, padding)
-                    AllSymbolsView(keyboardKey: keyboardKey, proxySize: proxySize)
-                    //                        .padding(.top, padding)
-                    //                        .padding(.bottom, padding + adjustment)
-                }
-            } else if keyboardKey.interval.intervalClass == .six {
+        let topBottomPadding = keyboardKey.layoutChoice == .symmetric && keyboardKey.interval.intervalClass != .six ? proxySize.width / 2.0 : 0.0
+        if keyboardKey.layoutChoice == .symmetric && keyboardKey.interval.consonanceDissonance > .consonant {
+            VStack(spacing: 0.0) {
                 AllSymbolsView(keyboardKey: keyboardKey, proxySize: proxySize)
-            } else {
+                    .padding([.top, .bottom], topBottomPadding)
                 AllSymbolsView(keyboardKey: keyboardKey, proxySize: proxySize)
-                //                    .padding(.top, padding - adjustment)
-                //                    .padding(.bottom, padding + adjustment)
+                    .padding([.top, .bottom], topBottomPadding)
             }
         } else {
             AllSymbolsView(keyboardKey: keyboardKey, proxySize: proxySize)
-            //                .padding(.top, padding - adjustment)
-            //                .padding(.bottom, padding - adjustment)
+                .padding([.top, .bottom], topBottomPadding)
         }
     }
-}
-
-struct AllSymbolsView: View {
     
-    var keyboardKey: KeyboardKey
-    var proxySize: CGSize
-    
-    var body: some View {
-        let anyIntervalLabels: Bool = keyboardKey.whichIntervalLabels.values.contains(true)
-        let anyNoteLabels: Bool = keyboardKey.whichNoteLabels.values.contains(true)
+    struct AllSymbolsView: View {
         
-        VStack(spacing: 0) {
-            if keyboardKey.layoutChoice == .piano {
-                Color.clear
-                Color.clear
-                Color.clear
-            }
+        var keyboardKey: KeyboardKey
+        var proxySize: CGSize
+        
+        var body: some View {
+            let anyIntervalLabels: Bool = keyboardKey.whichIntervalLabels.values.contains(true)
+            let anyNoteLabels: Bool = keyboardKey.whichNoteLabels.values.contains(true)
             
-            if anyNoteLabels || keyboardKey.showSymbols {
-                VStack(spacing: 1.0) {
-                    if keyboardKey.noteLabels[keyboardKey.layoutChoice]![.letter]! {
-                        Color.clear.overlay(
-                            Text(keyboardKey.pitch.letter)
-                        )
-                    }
-                    if keyboardKey.noteLabels[keyboardKey.layoutChoice]![.fixedDo]! {
-                        Color.clear.overlay(
-                            Text(keyboardKey.pitch.fixedDo)
-                        )
-                    }
-                    if keyboardKey.noteLabels[keyboardKey.layoutChoice]![.month]! {
-                        Color.clear.overlay(
-                            Text(Calendar.current.shortMonthSymbols[(keyboardKey.pitch.pitchClass.intValue + 3) % 12].uppercased())
-                        )
-                    }
-                    if keyboardKey.noteLabels[keyboardKey.layoutChoice]![.midi]! {
-                        Color.clear.overlay(
-                            Text(String(keyboardKey.pitch.midi))
-                        )
-                    }
-                    if keyboardKey.noteLabels[keyboardKey.layoutChoice]![.frequency]! {
-                        Color.clear.overlay(
-                            Text(pow(2, CGFloat(keyboardKey.pitch.midi - 69) / 12.0) * 440.0,
-                                 format: .number.notation(.compactName).precision(.fractionLength(1)))
-                        )
-                    }
-                }.frame(maxHeight: .infinity)
-            }
-            
-            if keyboardKey.showSymbols {
-                let symbolAdjustedLength = keyboardKey.symbolLength(proxySize)
-                VStack(spacing: 0) {
+            VStack(spacing: 0) {
+                if keyboardKey.layoutChoice == .piano {
+                    Color.clear
+                    Color.clear
+                    Color.clear
+                }
+                
+                if anyNoteLabels || keyboardKey.showSymbols {
+                    VStack(spacing: 1.0) {
+                        if keyboardKey.noteLabels[keyboardKey.layoutChoice]![.letter]! {
+                            Color.clear.overlay(
+                                Text(keyboardKey.pitch.letter)
+                            )
+                        }
+                        if keyboardKey.noteLabels[keyboardKey.layoutChoice]![.fixedDo]! {
+                            Color.clear.overlay(
+                                Text(keyboardKey.pitch.fixedDo)
+                            )
+                        }
+                        if keyboardKey.noteLabels[keyboardKey.layoutChoice]![.month]! {
+                            Color.clear.overlay(
+                                Text(Calendar.current.shortMonthSymbols[(keyboardKey.pitch.pitchClass.intValue + 3) % 12].uppercased())
+                            )
+                        }
+                        if keyboardKey.noteLabels[keyboardKey.layoutChoice]![.midi]! {
+                            Color.clear.overlay(
+                                Text(String(keyboardKey.pitch.midi))
+                            )
+                        }
+                        if keyboardKey.noteLabels[keyboardKey.layoutChoice]![.frequency]! {
+                            Color.clear.overlay(
+                                Text(pow(2, CGFloat(keyboardKey.pitch.midi - 69) / 12.0) * 440.0,
+                                     format: .number.notation(.compactName).precision(.fractionLength(1)))
+                            )
+                        }
+                    }.frame(maxHeight: .infinity)
+                }
+                
+                if keyboardKey.showSymbols {
+                    let symbolAdjustedLength = keyboardKey.symbolLength(proxySize)
                     VStack(spacing: 0) {
-                        SymbolView(keyboardKey: keyboardKey, proxySize: proxySize, width: symbolAdjustedLength)
+                        VStack(spacing: 0) {
+                            SymbolView(keyboardKey: keyboardKey, proxySize: proxySize, width: symbolAdjustedLength)
+                        }
                     }
+                    .frame(height: keyboardKey.maxSymbolLength(proxySize))
                 }
-                .frame(height: keyboardKey.maxSymbolLength(proxySize))
-            }
-            
-            if anyIntervalLabels  || keyboardKey.showSymbols {
-                VStack(spacing: 1.0) {
-                    if keyboardKey.intervalLabels[keyboardKey.layoutChoice]![.interval]! {
-                        Color.clear.overlay(
-                            Text(String(keyboardKey.interval.interval))
-                        )
+                
+                if anyIntervalLabels  || keyboardKey.showSymbols {
+                    VStack(spacing: 1.0) {
+                        if keyboardKey.intervalLabels[keyboardKey.layoutChoice]![.interval]! {
+                            Color.clear.overlay(
+                                Text(String(keyboardKey.interval.interval))
+                            )
+                        }
+                        if keyboardKey.intervalLabels[keyboardKey.layoutChoice]![.movableDo]! {
+                            Color.clear.overlay(
+                                Text(keyboardKey.interval.movableDo)
+                            )
+                        }
+                        if keyboardKey.intervalLabels[keyboardKey.layoutChoice]![.roman]! {
+                            Color.clear.overlay(
+                                Text(String(keyboardKey.interval.roman))
+                            )
+                        }
+                        if keyboardKey.intervalLabels[keyboardKey.layoutChoice]![.degree]! {
+                            Color.clear.overlay(
+                                Text(String(keyboardKey.interval.degree))
+                            )
+                        }
+                        if keyboardKey.intervalLabels[keyboardKey.layoutChoice]![.integer]! {
+                            Color.clear.overlay(
+                                Text(String(keyboardKey.interval.semitones))
+                            )
+                        }
                     }
-                    if keyboardKey.intervalLabels[keyboardKey.layoutChoice]![.movableDo]! {
-                        Color.clear.overlay(
-                            Text(keyboardKey.interval.movableDo)
-                        )
-                    }
-                    if keyboardKey.intervalLabels[keyboardKey.layoutChoice]![.roman]! {
-                        Color.clear.overlay(
-                            Text(String(keyboardKey.interval.roman))
-                        )
-                    }
-                    if keyboardKey.intervalLabels[keyboardKey.layoutChoice]![.degree]! {
-                        Color.clear.overlay(
-                            Text(String(keyboardKey.interval.degree))
-                        )
-                    }
-                    if keyboardKey.intervalLabels[keyboardKey.layoutChoice]![.integer]! {
-                        Color.clear.overlay(
-                            Text(String(keyboardKey.interval.semitones))
-                        )
-                    }
+                    .frame(maxHeight: .infinity)
                 }
-                .frame(maxHeight: .infinity)
+                
             }
-            
+            .font(.custom("Courier", size: 28))
+            .foregroundColor(keyboardKey.textColor)
+            .lineLimit(1)
+            .minimumScaleFactor(0.01)
+            .padding(3.0)
         }
-        .font(.custom("Courier", size: 28))
-        .foregroundColor(keyboardKey.textColor)
-        .lineLimit(1)
-        .minimumScaleFactor(0.01)
-        .padding(3.0)
+        
     }
     
-}
-
-struct SymbolView: View {
-    var keyboardKey: KeyboardKey
-    var proxySize: CGSize
-    var width: CGFloat
-    var body: some View {
-        ZStack {
-            Color.clear
-                .aspectRatio(1.0, contentMode: .fit)
-                .frame(width: keyboardKey.maxSymbolLength(proxySize))
-                .overlay(
-                    AnyShape(keyboardKey.keySymbol)
-                        .aspectRatio(1.0, contentMode: .fit)
-                        .foregroundColor(keyboardKey.symbolColor)
-                        .frame(width: width)
-                )
+    struct SymbolView: View {
+        var keyboardKey: KeyboardKey
+        var proxySize: CGSize
+        var width: CGFloat
+        var body: some View {
+            ZStack {
+                Color.clear
+                    .aspectRatio(1.0, contentMode: .fit)
+                    .frame(width: keyboardKey.maxSymbolLength(proxySize))
+                    .overlay(
+                        AnyShape(keyboardKey.keySymbol)
+                            .aspectRatio(1.0, contentMode: .fit)
+                            .foregroundColor(keyboardKey.symbolColor)
+                            .frame(width: width)
+                    )
+            }
         }
     }
 }
