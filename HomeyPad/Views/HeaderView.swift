891477d1d03dee50899c476389ad86a302c8ec02
Brian Mulloy <brian@landlessness.net>
2024-04-07 10:02:25 -0400

    fixed order of octave buttons

--- HomeyPad/Views/HeaderView.swift
+++ HomeyPad/Views/HeaderView.swift
@@ -9,11 +9,38 @@ import SwiftUI
 
 struct HeaderView: View {
     @StateObject var viewConductor: ViewConductor
-   
+    
     var body: some View {
-        HStack(alignment: .center, spacing: 0) {
-            TonicPickerView(viewConductor: viewConductor)
-            Spacer()
+        HStack {
+            HStack {
+                Image(systemName: "water.waves.and.arrow.down")
+                    .foregroundColor(.white)
+                    .font(Font.system(size: .leastNormalMagnitude, weight: .thin))
+                Text("0")
+                    .font(.custom("Courier", size: 20.0))
+                Image(systemName: "water.waves.and.arrow.up")
+                    .foregroundColor(.white)
+                    .font(Font.system(size: .leastNormalMagnitude, weight: .thin))
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.leading, viewConductor.headerFooterPadding)
+            HStack {
+                Image(systemName: "tag")
+                    .foregroundColor(.white)
+                    .font(Font.system(size: .leastNormalMagnitude, weight: .thin))
+                Image(systemName: "house.fill")
+                    .padding(30.0)
+                Image(systemName: "paintpalette")
+                    .foregroundColor(.white)
+                    .font(Font.system(size: .leastNormalMagnitude, weight: .thin))
+            }
+            HStack {
+                Image(systemName: "questionmark.circle")
+                    .foregroundColor(.white)
+                    .font(Font.system(size: .leastNormalMagnitude, weight: .thin))
+            }
+            .frame(maxWidth: .infinity, alignment: .trailing)
+            .padding(.trailing, viewConductor.headerFooterPadding)
         }
     }
 }
