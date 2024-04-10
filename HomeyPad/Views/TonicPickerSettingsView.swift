696a9d8fb10c74a813bbf4ea186ff8bef892fe08
Brian Mulloy <brian@landlessness.net>
2024-04-09 22:51:36 -0400

    fixed home alignment bug;

--- HomeyPad/Views/TonicPickerSettingsView.swift
+++ HomeyPad/Views/TonicPickerSettingsView.swift
@@ -31,7 +31,6 @@ struct TonicPickerSettingsView: View {
                     .scrollIndicatorsFlash(onAppear: true)
                 }
             })
-            .padding(.trailing, 10)        
 
             Button(action: {
                 withAnimation {
