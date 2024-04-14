0ad2805353c4ca77ed0fe3cd9eb2e2ac3e9032c4
Brian Mulloy <brian@landlessness.net>
2024-04-14 15:47:44 -0400

    sample is now playing. neededed to set AVAudioSession to .playback

--- HomeyPad/HomeyPadApp.swift
+++ HomeyPad/HomeyPadApp.swift
@@ -1,11 +1,21 @@
 import SwiftUI
+import AVFoundation
 
 @main
-struct HomeyPadApp: App {
+struct HomeyPad: App {
+    init() {
+#if os(iOS)
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback)
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch let err {
+            print(err)
+        }
+#endif
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
-
