96bdb42f891a8dd2116272c07fa35274c9e1830b
Brian Mulloy <brian@landlessness.net>
2024-04-14 21:42:46 -0400

    formatted help

--- HomeyPad/Views/HelpPopoverView.swift
+++ HomeyPad/Views/HelpPopoverView.swift
@@ -12,67 +12,155 @@ struct HelpPopoverView: View {
     static let mamiIcon: String = "paintbrush.pointed.fill"
     
     var body: some View {
-        Text("Homey Pad")
+        HStack(alignment: .center) {
+            Spacer()
+            Text("Homey Pad")
+                .font(.headline)
+            Spacer()
+        }
+        .padding(5)
         Divider()
+            .padding(.bottom, 3)
         ScrollView(.vertical) {
-            Text("Legend")
-            Text("Icons: Consonance-Dissonance")
-            ForEach(ConsonanceDissonance.allCases, id: \.self) { codi in
-                HStack {
-                    AnyShape(codi.symbol)
-                        .aspectRatio(1.0, contentMode: .fit)
-                        .foregroundColor(.white)
-                    Text(codi.label.capitalized)
+            Grid(alignment: .leading, horizontalSpacing: 3, verticalSpacing: 5) {
+                GridRow {
+                    Text("Legend")
+                        .font(.subheadline)
+                        .gridCellColumns(2)
                 }
-            }
-            Text("Colors: Major-Minor")
-            ForEach(MajorMinor.allCases, id: \.self) { mami in
-                HStack {
-                    Image(systemName: HelpPopoverView.mamiIcon)
-                        .foregroundColor(Color(mami.color))
-                    Text(mami.label.capitalized)
+                GridRow {
+                    Text("Icons: Consonance-Dissonance")
+                        .font(.caption)
+                        .gridCellColumns(2)
                 }
-            }
-            Text("Intervals")
-            ForEach(IntervalClass.allCases, id: \.self) { intervalClass in
-                HStack {
-                    AnyShape(intervalClass.interval.consonanceDissonance.symbol)
-                        .aspectRatio(1.0, contentMode: .fit)
-                        .foregroundColor(Color(intervalClass.interval.majorMinor.color))
-                    Text(intervalClass.interval.shorthand)
-                    Text(intervalClass.interval.label)
+                ForEach(ConsonanceDissonance.allCases, id: \.self) { codi in
+                    GridRow {
+                        AnyShape(codi.symbol)
+                            .aspectRatio(1.0, contentMode: .fit)
+                            .gridColumnAlignment(.center)
+                            .frame(width: 17, height: 17)
+                            .foregroundColor(.white)
+                        Text(codi.label.capitalized)
+                    }
                 }
-            }
-            Text("Chords")
-            ForEach(ChordShape.allCases, id: \.self) { chord in
-                HStack {
-                    Image(systemName: chord.icon)
-                        .aspectRatio(1.0, contentMode: .fit)
-                        .foregroundColor(Color(chord.majorMinor.color))
-                    Text(chord.rawValue)
+                GridRow {
+                    Text("Colors: Major-Minor")
+                        .font(.caption)
+                        .gridCellColumns(2)
                 }
-            }
-            Text("Pitch Directions")
-            ForEach(PitchDirection.allCases, id: \.self) { pitchDirection in
-                HStack {
-                    Image(systemName: pitchDirection.icon)
-                        .aspectRatio(1.0, contentMode: .fit)
-                        .foregroundColor(Color(pitchDirection.majorMinor.color))
-                    Text(pitchDirection.label)
+                ForEach(MajorMinor.allCases, id: \.self) { mami in
+                    GridRow {
+                        Image(systemName: HelpPopoverView.mamiIcon)
+                            .gridColumnAlignment(.center)
+                            .foregroundColor(Color(mami.color))
+                        Text(mami.label.capitalized)
+                    }
                 }
-            }
-            Text("Modes")
-            ForEach(Mode.allCases, id: \.self) { mode in
-                HStack {
-                    Image(systemName: mode.pitchDirection.icon)
-                        .aspectRatio(1.0, contentMode: .fit)
-                        .foregroundColor(Color(mode.pitchDirection.majorMinor.color))
-                    Image(systemName: mode.chordShape.icon)
-                        .aspectRatio(1.0, contentMode: .fit)
-                        .foregroundColor(Color(mode.chordShape.majorMinor.color))
-                    Text("\(mode.letter) \(mode.label)")
+                GridRow {
+                    Text("Intervals")
+                        .font(.caption)
+                        .gridCellColumns(2)
                 }
+                ForEach(IntervalClass.allCases, id: \.self) { intervalClass in
+                    GridRow {
+                        AnyShape(intervalClass.interval.consonanceDissonance.symbol)
+                            .gridColumnAlignment(.center)
+                            .aspectRatio(1.0, contentMode: .fit)
+                            .frame(width: 17, height: 17)
+                            .foregroundColor(Color(intervalClass.interval.majorMinor.color))
+                        Text("\(intervalClass.interval.shorthand) \(intervalClass.interval.label.capitalized)")
+                    }
+                }
+                GridRow {
+                    Text("Chords")
+                        .font(.caption)
+                        .gridCellColumns(2)
+                }
+                ForEach(ChordShape.allCases, id: \.self) { chord in
+                    GridRow {
+                        Image(systemName: chord.icon)
+                            .gridColumnAlignment(.center)
+                            .aspectRatio(1.0, contentMode: .fit)
+                            .frame(width: 17, height: 17)
+                            .foregroundColor(Color(chord.majorMinor.color))
+                        Text(chord.rawValue.capitalized)
+                    }
+                }
+                GridRow {
+                    Text("Pitch Directions")
+                        .font(.caption)
+                        .gridCellColumns(2)
+                }
+                ForEach(PitchDirection.allCases, id: \.self) { pitchDirection in
+                    GridRow {
+                        Image(systemName: pitchDirection.icon)
+                            .gridColumnAlignment(.center)
+                            .aspectRatio(1.0, contentMode: .fit)
+                            .foregroundColor(Color(pitchDirection.majorMinor.color))
+                        Text(pitchDirection.label.capitalized)
+                    }
+                }
+                GridRow {
+                    Text("Modes")
+                        .font(.caption)
+                        .gridCellColumns(2)
+                }
+                ForEach(Mode.allCases, id: \.self) { mode in
+                    GridRow {
+                        HStack(spacing: 0) {
+                            Image(systemName: "square")
+                                .foregroundColor(.clear)
+                                .overlay(
+                                    Image(systemName: mode.pitchDirection.icon)
+                                        .aspectRatio(1.0, contentMode: .fit)
+                                        .foregroundColor(Color(mode.pitchDirection.majorMinor.color))
+                                )
+                            Image(systemName: "square")
+                                .foregroundColor(.clear)
+                                .overlay(
+                                    Image(systemName: mode.chordShape.icon)
+                                        .aspectRatio(1.0, contentMode: .fit)
+                                        .foregroundColor(Color(mode.chordShape.majorMinor.color))
+                                )
+                        }
+                        .gridColumnAlignment(.center)
+                        Text("\(mode.letter) \(mode.label.capitalized)")
+                    }
+                }
+                Divider()
+                    .padding(3)
+                Group {
+                    GridRow {
+                        Text("Legal")
+                            .font(.subheadline)
+                            .gridCellColumns(2)
+                    }
+                    GridRow {
+                        Text("The design of Homey Pad including layout, color palette, user interactions and icon language is released under an [Attribution 4.0 International license (CC BY 4.0)](https://creativecommons.org/licenses/by/4.0/).")
+                            .gridCellColumns(2)
+                    }
+                    GridRow {
+                        (
+                            Text("The Homey Pad software is released under an [MIT License](https://opensource.org/license/mit/) and is available on [GitHub](https://github.com/homeymusic/homeypad).")
+                        )
+                        .gridCellColumns(2)
+                    }
+                    GridRow {
+                        (
+                            Text(Image(systemName: "c.circle")) +
+                            Text(" 2023-2024 [Homey Music](https://homeymusic.com), a registered trade name of Ballet, LLC. All other rights reserved. Homey Pad is a product name and trademark of [Homey Music](https://homeymusic.com).")
+                        )
+                        .gridCellColumns(2)
+                    }
+                    GridRow {
+                        Text("The Yamaha Disklavier Pro soundfont was produced by [Zenvoid](https://freepats.zenvoid.org/Piano/acoustic-grand-piano.html) from samples made by [Zenph Studios](https://www.nytimes.com/2007/03/12/arts/music/12conn.html) for [One Laptop Per Child](http://wiki.laptop.org/go/Sound_samples) and is used by Homey Pad under a [Creative Commons Attribution 3.0 license](https://creativecommons.org/licenses/by/3.0/).")
+                            .gridCellColumns(2)
+                    }
+                }
+                .font(.footnote)
             }
+            .frame(width: 250)
+            .padding(10)
         }
     }
 }
