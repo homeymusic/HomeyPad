2ce0d587027d26793c2cae342f6aedd1d392bdd0
Brian Mulloy <brian@landlessness.net>
2024-04-18 07:41:29 -0400

    back to sublte tonic default

--- HomeyPad/Keyboard/Layouts/LayoutPalette.swift
+++ HomeyPad/Keyboard/Layouts/LayoutPalette.swift
@@ -2,7 +2,7 @@ struct LayoutPalette: Codable, Equatable {
     var choices: [LayoutChoice: PaletteChoice] = defaultLayoutPalette
     
     static let defaultLayoutPalette: [LayoutChoice: PaletteChoice] = [
-        .tonic:      .loud,
+        .tonic:      .subtle,
         .isomorphic: .subtle,
         .symmetric:  .subtle,
         .piano:      .subtle,
