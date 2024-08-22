c2b54b51c2caf1c33dd6b21c165c9adeec656101
Brian Mulloy <brian@landlessness.net>
2024-08-22 17:29:06 -0400

    changed colors of app icon and major minor within the app

--- HomeyPad/MusicTheory/MajorMinor.swift
+++ HomeyPad/MusicTheory/MajorMinor.swift
@@ -17,9 +17,9 @@ enum MajorMinor: Int, CaseIterable, Identifiable, Comparable, Equatable {
     
     var color: CGColor {
         switch self {
-        case .minor:   return #colorLiteral(red: 0.5411764706, green: 0.7725490196, blue: 1, alpha: 1)
+        case .minor:   return #colorLiteral(red: 0.3647058824, green: 0.6784313725, blue: 0.9254901961, alpha: 1)
         case .neutral: return #colorLiteral(red: 0.9529411765, green: 0.8666666667, blue: 0.6705882353, alpha: 1)
-        case .major:   return #colorLiteral(red: 1, green: 0.6901960784, blue: 0, alpha: 1)
+        case .major:   return #colorLiteral(red: 1, green: 0.6745098039, blue: 0.2, alpha: 1)
         }
     }
 
@@ -31,7 +31,7 @@ enum MajorMinor: Int, CaseIterable, Identifiable, Comparable, Equatable {
         }
     }
 
-    static let altNeutralColor: CGColor =  #colorLiteral(red: 1, green: 0.3333333333, blue: 0, alpha: 1)
+    static let altNeutralColor: CGColor =  #colorLiteral(red: 1, green: 0.333333, blue: 0, alpha: 1)
     
     static func < (lhs: MajorMinor, rhs: MajorMinor) -> Bool {
         lhs.rawValue < rhs.rawValue
