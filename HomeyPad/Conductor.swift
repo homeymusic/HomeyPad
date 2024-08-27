d2a42e10dcaaf9d2b3394664f456a3aa2ae1e808
Brian Mulloy <brian@landlessness.net>
2024-08-27 19:39:21 -0400

    changed audio to mix with other sounds

--- HomeyPad/Conductor.swift
+++ HomeyPad/Conductor.swift
@@ -4,17 +4,29 @@ class Conductor: ObservableObject {
     let engine = AVAudioEngine()
     var instrument = AVAudioUnitSampler()
     
+    func configureAudioSession() {
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            // Set the category to allow mixing with other audio
+            try audioSession.setCategory(.playback, options: [.mixWithOthers])
+            try audioSession.setActive(true)
+        } catch {
+            print("Error setting up audio session: \(error)")
+        }
+    }
+    
     init() {
+        configureAudioSession()
         engine.attach(instrument)
         engine.connect(instrument, to: engine.mainMixerNode, format: nil)
         start()
     }
-
+    
     func start() {
         try? instrument.loadInstrument(at: Bundle.main.url(forResource: "Sounds/YDP-GrandPiano-20160804", withExtension: "exs")!)
         try? engine.start()
     }
-
+    
     func noteOn(pitch: Pitch) {
         instrument.startNote(UInt8(pitch.midi), withVelocity: 64, onChannel: 0)
     }
